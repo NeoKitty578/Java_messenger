ce3dee8edc2b1f5edb24c05dad8132c3378b161f
NeoKitty578 <ST10507528@rcconnect.edu.za>
2026-09-08 15:20:02 +0200

    Added methods and defined them by the Login class

--- src/javamessengerapplication/Login.java
+++ src/javamessengerapplication/Login.java
@@ -12,11 +12,55 @@ package javamessengerapplication;
 // This class will have methods that will validate user input
 public class Login {
    
-    public boolean checkUserName(String fUsername){
-        if ((fUsername.length() <=5) && (fUsername.contains("_"))){
-            System.out.println("Username successfully captured.");
-            }else{
-                
-        }
+    //
+    public static boolean checkUserName(String username){
+        if (username == null) return false;
+        return username.contains("_") && username.length()<=5;
     }
+    //the following method checks if the password meets the requirements.
+    //password requirements
+    /*At least 8 characters long
+    *Contains a capital letter
+    *Contains a number
+    *Contains a special character
+    */
+    public static boolean checkPasswordComplexity(String password){
+        //if the password is empty or doesnt meet at least 8 characters then return false
+        if (password == null || password.length()<8) return false;
+        
+        //variables to check a capital letter, a number, and a speacial character
+        boolean hasCapLetter = false;
+        boolean hasNumber = false;
+        boolean hasChar = false;
+        
+        for (int i = 0; i<password.length(); i++){
+              //checking if there's a Uppercase letter in the password
+              char k = password .charAt(i);
+              
+              //now checking if the password conditions are met
+              if (Character.isUpperCase(k)){
+                  hasCapLetter = true;
+              }
+              //checking digits
+              if (Character.isDigit(k)){
+                  hasNumber = true;
+              }
+              //checking if there's special characters excluding space
+              if (!Character.isLetterOrDigit(k) && !Character.isWhitespace(k)){
+                hasChar = true;
+            }
+            }
+        return hasCapLetter && hasChar && hasNumber;
+    }
+    
+    //
+    public static boolean checkCellPhoneNumber(String phonenumber){
+       //checking if the password meets the standard format
+       if (phonenumber == null)return false;
+       //prefixes = 06,07,08, with or without the plus sign
+            String standardRegex = "^(?:\\+27|27|0)[678]\\d{8}$"; 
+            
+            if (phonenumber.matches(standardRegex))return true;
+    }
+    
 }
